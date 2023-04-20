e2a3ad9eb749568801e140a669ecfb8bf735f4a3
romitkumark <romitkumark@gmail.com>
2023-04-20 15:09:06 +0530

    UI changes

--- resources/views/file-upload.blade.php
+++ resources/views/file-upload.blade.php
@@ -22,13 +22,13 @@
             </ul>
         </h6>
 
-        <div class="form-group">
+        <div class="form-group" style="border-style: dashed;">
             <form method="POST" enctype="multipart/form-data" id="upload-file" action="{{ url('store') }}" onSubmit="return submitFileUpload();">
                         
                 <div class="row">
 
                     <div class="col-md-12">
-                        <div class="form-group">
+                        <div class="form-group" style="padding: 10px;">
                             <input type="file" name="file" placeholder="Choose file" id="file">
                                 @error('file')
                                 <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
@@ -37,7 +37,9 @@
                     </div>
                         
                     <div class="col-md-12">
-                        <button type="submit" class="btn btn-primary" id="submit">Submit</button>
+                        <div class="form-group" style="padding: 10px;">
+                            <button type="submit" class="btn btn-primary" id="submit">Submit</button>
+                        </div>
                     </div>
                 </div>
             </form>
